f618fb3b2d426471dd44ff5468dab01aff401ef0
vagrant <vagrant@homestead>
2017-09-27 17:09:38 +0000

    adjusted contact styling. contact information is now correct and copyable.

--- public/js/videos/script.js
+++ public/js/videos/script.js
@@ -125,7 +125,7 @@ function Player(videos){
 
     //array of videos [loader, home, projecten, watDoenWijAnders, contact]
     this.videos                         = videos;
-    this.currentVideo                   = videos[3];
+    this.currentVideo                   = videos[4];
     this.currentVideoPiece              = 'main';
 
     //players
