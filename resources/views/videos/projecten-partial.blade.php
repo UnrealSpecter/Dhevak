5b6287d36c6af80b91fec9788b3e74dd191df72d
vagrant <vagrant@homestead>
2017-10-02 14:58:09 +0000

    started working on the project content

--- resources/views/videos/projecten-partial.blade.php
+++ resources/views/videos/projecten-partial.blade.php
@@ -3,8 +3,40 @@
     <!-- projecten menu -->
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 projecten-menu" onclick="openNav()"></div>
 
-    <div class="projecten-left"></div>
-    <div class="projecten-middle"></div>
-    <div class="projecten-right"></div>
+    <div class="project projecten-left"></div>
+    <div class="project projecten-middle"></div>
+    <div class="project projecten-right"></div>
+
+    <div class="project-overlay invisible">
+        <div class="project-bg-title center">PROJECT TITLE</div>
+        <div class="project-title center">PROJECT TITLE</div>
+        <div class="project-website center">WWW.PROJECT-WEBSITE.NL</div>
+        <div class="project-social-media-wrapper">
+            <div class="flex project-social-media-link">
+                <a href="" target="_blank" style="color: white;">FACEBOOK</a>
+            </div>
+            <div class="flex project-social-media-link">
+                <a href="" target="_blank" style="color: white;">TWITTER</a>
+            </div>
+            <div class="flex project-social-media-link">
+                <a href="" target="_blank" style="color: white;">INSTAGRAM</a>
+            </div>
+            <div class="flex project-social-media-link">
+                <a href="" target="_blank" style="color: white;">LINKEDIN</a>
+            </div>
+        </div>
+        <!-- <img class="img-responsive" src="{{ URL::asset('/images/projecten-test-image.jpg') }}"> -->
+
+        <!-- <div class="project-description flex animated slideInLeft">PROJECT DESCRIPTION
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mihi enim satis est, ipsis non satis. Diodorus, eius auditor, adiungit ad honestatem vacuitatem doloris.
+
+    Non ego tecum iam ita iocabor, ut isdem his de rebus, cum L. Utilitatis causa amicitia est quaesita. Ergo, si semel tristior effectus est, hilara vita amissa est? Ac tamen hic mallet non dolere. Quod idem cum vestri faciant, non satis magnam tribuunt inventoribus gratiam. Quod quidem iam fit etiam in Academia.
+
+    Expressa vero in iis aetatibus, quae iam confirmatae sunt. Cur iustitia laudatur? Verum tamen cum de rebus grandioribus dicas, ipsae res verba rapiunt; Uterque enim summo bono fruitur, id est voluptate. Etenim nec iustitia nec amicitia esse omnino poterunt, nisi ipsae per se expetuntur. Mihi enim erit isdem istis fortasse iam utendum. Quod ea non occurrentia fingunt, vincunt Aristonem;
+
+    Scio enim esse quosdam, qui quavis lingua philosophari possint; Duo Reges: constructio interrete. Contemnit enim disserendi elegantiam, confuse loquitur. Etenim semper illud extra est, quod arte comprehenditur.
+
+    Sunt autem, qui dicant foedus esse quoddam sapientium, ut ne minus amicos quam se ipsos diligant. Non est ista, inquam, Piso, magna dissensio. Possumusne ergo in vita summum bonum dicere, cum id ne in cena quidem posse videamur? Graece donan, Latine voluptatem vocant. Quid censes in Latino fore? Qui potest igitur habitare in beata vita summi mali metus?</div>
+    </div> -->
 
 </div>
--- resources/views/videos/wat-doen-wij-anders-partial.blade.php
+++ resources/views/videos/wat-doen-wij-anders-partial.blade.php
@@ -1,18 +1,13 @@
 <!-- wat-doen-wij-anders-partial -->
 <div id="content-wrapper" class="wat-doen-wij-anders animated fadeIn hidden">
-
     <div class="theater-ambiance fadeIn" style="background-color: black; opacity: 0.4; height: 100%; with: 100%;"></div>
-
     <div class="wat-doen-wij-anders-explanation-video">
-
         <video controls preload="auto" class="explanation-video" id="explanation-video" poster="{{ URL::asset('/images/posters/wat-doen-wij-anders/wat-doen-wij-anders-explanation-video-poster.jpg') }}">
             <source id="pre-intro-left-source" src="{{ URL::asset('/videos/wat-doen-wij-anders/wat-doen-wij-explanation-video.mp4')}}">
         </video>
-
     </div>
-
     <div class="video-controls">
         <!-- wat-doen-wij-anders menu -->
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 wat-doen-wij-anders-menu" class="fullscreen-video" onclick="openNav()" style="height: 100%; width: 20%; color: black; display: flex; align-items: center; justify-content:center;"> menu </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 wat-doen-wij-anders-menu" class="fullscreen-video" onclick="openNav()"> MENU </div>
     </div>
 </div>
