5ef040786a36f1ac6ae2bd1a659c968324f486dc
vagrant <vagrant@homestead>
2017-10-09 17:50:20 +0000

    loader work

--- resources/views/partials/videos/projecten-videos.blade.php
+++ resources/views/partials/videos/projecten-videos.blade.php
@@ -1,18 +1,18 @@
-<video muted class="player" id="pre-intro-left">
-    <source id="pre-intro-left-source" src="">
+<video muted class="player projecten" id="pre-intro-left">
+    <source id="pre-intro-left-source" src="/videos/projecten/projecten-pre-intro-left.mp4">
 </video>
-<video muted class="player" id="post-intro-left">
-    <source id="post-intro-left-source" src="">
+<video muted class="player projecten" id="post-intro-left">
+    <source id="post-intro-left-source" src="/videos/projecten/projecten-post-intro-left.mp4">
 </video>
-<video muted class="player" id="post-intro-right">
-    <source id="post-intro-right-source" src="">
+<video muted class="player projecten" id="post-intro-right">
+    <source id="post-intro-right-source" src="/videos/projecten/projecten-post-intro-right.mp4">
 </video>
-<video muted class="player" id="main">
-    <source id="main-source" src="">
+<video muted class="player projecten" id="main">
+    <source id="main-source" src="/videos/projecten/projecten-main.mp4">
 </video>
-<video muted class="player" id="outro-left">
-    <source id="outro-left-source" src="">
+<video muted class="player projecten" id="outro-left">
+    <source id="outro-left-source" src="/videos/projecten/projecten-outro-left.mp4">
 </video>
-<video muted class="player" id="outro-right">
-    <source id="outro-right-source" src="">
+<video muted class="player projecten" id="outro-right">
+    <source id="outro-right-source" src="/videos/projecten/projecten-outro-right.mp4">
 </video>
--- resources/views/partials/videos/wat-doen-wij-anders-videos.blade.php
+++ resources/views/partials/videos/wat-doen-wij-anders-videos.blade.php
@@ -1,18 +1,18 @@
-<video preload="auto" muted class="player" id="pre-intro-left">
-    <source id="pre-intro-left-source" src="">
+<video muted class="player wat-doen-wij-anders" id="pre-intro-left">
+    <source id="pre-intro-left-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-pre-intro-left.mp4">
 </video>
-<video preload="auto" muted class="player" id="post-intro-left">
-    <source id="post-intro-left-source" src="">
+<video muted class="player wat-doen-wij-anders" id="post-intro-left">
+    <source id="post-intro-left-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-post-intro-left.mp4">
 </video>
-<video preload="auto" muted class="player" id="post-intro-right">
-    <source id="post-intro-right-source" src="">
+<video muted class="player wat-doen-wij-anders" id="post-intro-right">
+    <source id="post-intro-right-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-post-intro-right.mp4">
 </video>
-<video preload="auto" muted class="player" id="main">
-    <source id="main-source" src="">
+<video muted class="player wat-doen-wij-anders" id="main">
+    <source id="main-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-main.mp4">
 </video>
-<video preload="auto" muted class="player" id="outro-left">
-    <source id="outro-left-source" src="">
+<video muted class="player wat-doen-wij-anders" id="outro-left">
+    <source id="outro-left-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-outro-left.mp4">
 </video>
-<video preload="auto" muted class="player" id="outro-right">
-    <source id="outro-right-source" src="">
+<video muted class="player wat-doen-wij-anders" id="outro-right">
+    <source id="outro-right-source" src="/videos/wat-doen-wij-anders/wat-doen-wij-anders-outro-right.mp4">
 </video>
