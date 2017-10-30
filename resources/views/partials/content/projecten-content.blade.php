fc3bce6bb8dcbe094d6dabcaee1711183e32e810
unrealspecter <tomi_emmen@hotmail.com>
2017-10-30 15:57:28 +0000

    project content work

--- resources/views/partials/content/projecten-content.blade.php
+++ resources/views/partials/content/projecten-content.blade.php
@@ -4,15 +4,21 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 projecten-menu" onclick="openNav()"></div>
 
     <!-- projecten-image -->
-    <div class="project projecten-left" data-project="1">
-        <img src="{{ URL::asset('/images/projecten-test-image.jpg') }}" alt="{{ project->title }}">
-    </div>
-    <div class="project projecten-middle" data-project="2">
-        <img src="{{ URL::asset('/images/projecten-test-image.jpg') }}" alt="{{ project->title }}">
-    </div>
-    <div class="project projecten-right" data-project="3">
-        <img src="{{ URL::asset('/images/projecten-test-image.jpg') }}" alt="{{ project->title }}">
-    </div>
+    @foreach($projects as $index => $project)
+        @if($index % 3 === 1)
+        <div class="project projecten-left d-none" data-project="{{ $index }}">
+            <img src="{{ $project->thumbnail_image_url }}" alt="{{ $project->title }}">
+        </div>
+        @elseif($index % 3 === 2)
+        <div class="project projecten-middle d-none" data-project="{{ $index }}">
+            <img src="{{ $project->thumbnail_image_url }}" alt="{{ $project->title }}">
+        </div>
+        @elseif($index % 3 === 0)
+        <div class="project projecten-right d-none" data-project="{{ $index }}">
+            <img src="{{ asset('') }}" alt="{{ $project->title }}">
+        </div>
+        @endif
+    @endforeach
 
     <!-- project navigation -->
     <div class="arrow-up next-projects" data-direction="previous-projects"></div>
