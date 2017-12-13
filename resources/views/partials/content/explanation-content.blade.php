7184d64fd71a389ab241c3d8ca3e451d72d44c12
vagrant <vagrant@homestead>
2017-12-13 23:48:23 +0000

    swipe icon alt added

--- resources/views/partials/content/explanation-content.blade.php
+++ resources/views/partials/content/explanation-content.blade.php
@@ -6,7 +6,7 @@
 
         <div class="mobile-explanation flex flow-r d-none">
             <div class="animated fadeInDown"> Navigeer door te swipen </div>
-            <img class="explanation-icon img-fluid" src="{{ asset('/images/explanation-images/swipe-icon.jpg') }}">
+            <img class="explanation-icon img-fluid" src="{{ asset('/images/explanation-images/swipe-icon.jpg') }}" alt="swipe-icon">
             <div class="animated fadeInUp explanation-confirm">Ok, Weg hiermee</div>
         </div>
 
