59875cf640ae3bd8b3b74c0970ab0eb20c07d9aa
vagrant <vagrant@homestead>
2017-12-18 15:18:24 +0000

    added analytics to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,6 +32,16 @@
         <!-- IE polyfill -->
         <script src="{{ URL::asset('plugins/object-fit-polyfill/js/objectFitPolyfill.min.js') }}"></script>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90310031-5"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-90310031-5');
+        </script>
+
         @yield('css')
 
         @yield('js')
